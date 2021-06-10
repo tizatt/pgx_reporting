eb9dbeacc0716f5596e44c70b115907012346b20
tizatt <tizatt@tgen.org>
2021-06-10 14:51:06 -0700

    Updated pharmcat to 0.8.0

--- pharmcat_astrolabe_run.sh
+++ pharmcat_astrolabe_run.sh
@@ -13,10 +13,12 @@ VCF=$2
 OUT=$3 # 
 OUTDIR=$4
 
-BED=/home/tizatt/pgx/pharmcat_convert/pharmcat.bed
+#BED=/home/tizatt/pgx/pharmcat_convert/pharmcat.bed
+BED=/home/tizatt/pgx/pharmcat_convert/pharmcat_intervals_0.8.0.txt
 ASTROLABE_SH=/home/tizatt/pgx/astrolabe_runs/run.sh
 BCFTOOLS_CALL=/home/tizatt/pgx/pharmcat_convert/bcftools_call.sh
-PHARMCAT_JAR=/home/tizatt/pgx/tools/PharmCAT/build/libs/pharmcat-0.7.1-all.jar
+#PHARMCAT_JAR=/home/tizatt/pgx/tools/PharmCAT/build/libs/pharmcat-0.7.1-all.jar
+PHARMCAT_JAR=/home/tizatt/pgx/tools/pharmcat-0.8.0-all.jar
 ASTROLABE_TSV=${OUTDIR}/${OUT}.astrolabe.tsv
 PHARMCAT_VCF=${OUTDIR}/${OUT}.pharmcat.vcf
 REF=/home/tgenref/homo_sapiens/grch38_hg38/hg38tgen/genome_reference/GRCh38tgen_decoy_alts_hla.fa
@@ -34,7 +36,9 @@ ${BCFTOOLS_CALL} ${BAM} ${BED} ${PHARMCAT_VCF} ${REF}
 
 # Run pharmcat
 
-java -jar ${PHARMCAT_JAR} -a ${ASTROLABE_TSV} -f ${OUT} -j -o ${OUTDIR}/${OUT} -vcf ${PHARMCAT_VCF}
+#java -jar ${PHARMCAT_JAR} -a ${ASTROLABE_TSV} -f ${OUT} -j -o ${OUTDIR}/${OUT} -vcf ${PHARMCAT_VCF}
+
+java -jar ${PHARMCAT_JAR} -f ${OUT} -j -o ${OUTDIR}/${OUT} -vcf ${PHARMCAT_VCF}
 
 
 
